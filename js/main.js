83c872fd0b648588aaf9472b6b0d3e8f3f786a17
Jbuckley <buckleyjosh3@gmail.com>
2023-11-20 21:05:44 -0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 console.log('js is linked and ready');
+
+//game displays a 3x3 grid
+/*Display an empty tic-tac-toe board when the page is initially displayed.
+
+A player can click on the nine cells to make a move.
+
+Every click will alternate between marking an X and O.
+
+Once occupied with an X or O, the cell cannot be played again.
+
+Provide a Reset Game button that will clear the contents of the board.*/
